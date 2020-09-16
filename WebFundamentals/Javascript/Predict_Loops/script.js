bc196cfc85e40f0d16885502b03900b9b211588c
Ian Rones <ronesp.85@gmail.com>
2020-09-16 14:43:50 -0700

    [add] predict the output - loops challlenge completed

---
+++ WebFundamentals/Javascript/Predict_Loops/script.js
@@ -0,0 +1,38 @@
+// Predict 1
+for(var num = 0; num < 15; num++){
+    console.log(num);
+}
+/*
+    Prediction
+     -> 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14
+*/
+
+// Predict 2
+for (var i = 1; i < 10; i += 2){
+    if (i % 3 == 0){
+        console.log(i);
+    }
+}
+
+/**
+    Prediction
+     ->  3,9
+ */
+
+ // Predict
+ for(var j = 1; j <= 15; j++){
+    if(j % 2 == 0){
+        j+=2;
+        console.log('Divided by 2');
+    }
+    else if(j % 3 == 0){
+        j++;
+        console.log('Divided by 3');
+    }
+    console.log(j);
+}
+
+/*
+    Prediction
+    -> 1,2,4,5,8,10,11,14,16
+*/
