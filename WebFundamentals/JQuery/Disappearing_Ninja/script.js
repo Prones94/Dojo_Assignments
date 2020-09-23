c0717698aa9133bba3004f8e20582d8ba29afd16
Ian Rones <ronesp.85@gmail.com>
2020-09-23 13:28:22 -0700

    [add] disappearing ninjas challenge completed

---
+++ WebFundamentals/JQuery/Disappearing_Ninja/script.js
@@ -0,0 +1,30 @@
+$("#cat").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_2").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_3").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_4").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_5").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_6").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_7").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+$("#cat_8").on('click',function(){
+    $(this, 'img').hide('slow')
+})
+
+$("#show").on("click", function(){
+    $(".cats").each(function(){
+        $(this, 'img').show('slow')
+    })
+})
