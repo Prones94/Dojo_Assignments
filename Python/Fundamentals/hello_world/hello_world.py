1f0355b6c2ee5be3a39063d858f2cec36932d512
Ian Rones <ronesp.85@gmail.com>
2020-09-28 10:11:52 -0700

    [add] hello_world.py completed

---
+++ Python/Fundamentals/hello_world/hello_world.py
@@ -0,0 +1,13 @@
+print(f'Hello World')
+
+name = "Ian Rones"
+print('Hello, ' + name)
+print("Hello, {}!".format(name))
+print('Hello',name)	# with a comma
+num = 42
+print('Hello', num)	# with a comma
+# print('Hello', + number)	# with a +	-- this one should give us an error!
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print('I love to eat {} and {}.'.format(fave_food1,fave_food2)) # with .format()
+print(f'I love to eat {fave_food1} and {fave_food2}') # with an f string
