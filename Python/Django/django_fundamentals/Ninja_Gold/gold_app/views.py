9381e605fb8fe468bef9beec8116c333372a06ea
Ian Rones <ronesp.85@gmail.com>
2020-10-05 23:12:58 -0700

    [add] ninja gold challenge almsot comopleted, working on log

--- Python/Django/django_fundamentals/Ninja_Gold/gold_app/views.py
+++ Python/Django/django_fundamentals/Ninja_Gold/gold_app/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
+import random 
 
-# Create your views here.
+def index(request):
+    print('******************************************')
+    if 'money' not in request.session:
+        request.session['money'] = 0
+        request.session['activities'] = []
+        # request.session['actions'] = ''
+    return render(request, "index.html")
+
+
+def process(request):
+    print('------------------------------------------')
+    if request.POST['options'] == 'farm':
+        # print(request.session['actions'])
+        money = random.randint(10,20)
+        request.session['money'] += money
+        action = "Earned money \n"
+        print('Milk money')
+    elif request.POST['options'] == 'cave':
+        money = random.randint(5,10)
+        request.session['money'] += money
+        # request.session['actions'] += "Earned " + str(gold) + " gold mining! " + "(" + str(datetime.now()) + ")\n"
+        print('Cavemen money')
+    elif request.POST['options'] == 'house':
+        money = random.randint(2,5)
+        request.session['money'] += money
+        # request.session['actions'] += "Earned " + str(gold) + " gold wfh! " + "(" + str(datetime.now()) + ")\n"
+        print('WFH')
+    elif request.POST['options'] == 'casino':
+        money = random.randint(-50, 50)
+        request.session['money'] += money
+        # if money > 0:
+            # request.session['actions'] += "Earned " + str(gold) + " gold gambling! " + "(" + str(ddatetime.now()) + ")\n"
+        # else:
+            # request.session['actions'] += "Lost " + str(gold) + " gold gambling! " + "(" + str(datetime.now()) + ")\n"
+        print('Las Vegas')
+    request.session['activities'].append("Hello\n")
+    request.session.save()
+    return redirect("/")
