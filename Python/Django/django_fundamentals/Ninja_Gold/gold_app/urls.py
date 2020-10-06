a11c9337bd78cc877a935df3153ef119a3f77025
Ian Rones <ronesp.85@gmail.com>
2020-10-06 14:04:42 -0700

    [edit] ninja gold list is functional, added reset button

--- Python/Django/django_fundamentals/Ninja_Gold/gold_app/urls.py
+++ Python/Django/django_fundamentals/Ninja_Gold/gold_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('process', views.process),
+    path('reset', views.reset),
 ]
--- Python/Django/django_fundamentals/Ninja_Gold/gold_app/views.py
+++ Python/Django/django_fundamentals/Ninja_Gold/gold_app/views.py
@@ -6,37 +6,44 @@ def index(request):
     print('******************************************')
     if 'money' not in request.session:
         request.session['money'] = 0
+    if 'activities' not in request.session:
         request.session['activities'] = []
-        # request.session['actions'] = ''
+    # context = {
+    #     'activities' : request.session['activities'],
+    # }
     return render(request, "index.html")
 
 
 def process(request):
     print('------------------------------------------')
     if request.POST['options'] == 'farm':
-        # print(request.session['actions'])
         money = random.randint(10,20)
         request.session['money'] += money
-        action = "Earned money \n"
+        string = "Earned money farming!" + "(" + str(datetime.now()) + ")\n"
         print('Milk money')
     elif request.POST['options'] == 'cave':
         money = random.randint(5,10)
         request.session['money'] += money
-        # request.session['actions'] += "Earned " + str(gold) + " gold mining! " + "(" + str(datetime.now()) + ")\n"
+        string= "Earned " + str(money) + " gold mining! " + "(" + str(datetime.now()) + ")\n"
         print('Cavemen money')
     elif request.POST['options'] == 'house':
         money = random.randint(2,5)
         request.session['money'] += money
-        # request.session['actions'] += "Earned " + str(gold) + " gold wfh! " + "(" + str(datetime.now()) + ")\n"
+        string = "Earned " + str(money) + " gold wfh! " + "(" + str(datetime.now()) + ")\n"
         print('WFH')
     elif request.POST['options'] == 'casino':
         money = random.randint(-50, 50)
         request.session['money'] += money
-        # if money > 0:
-            # request.session['actions'] += "Earned " + str(gold) + " gold gambling! " + "(" + str(ddatetime.now()) + ")\n"
-        # else:
-            # request.session['actions'] += "Lost " + str(gold) + " gold gambling! " + "(" + str(datetime.now()) + ")\n"
+        if money > 0:
+            string = "Earned " + str(money) + " gold gambling! " + "(" + str(datetime.now()) + ")\n"
+        else:
+            
+            string = "Lost " + str(money) + " gold gambling! " + "(" + str(datetime.now()) + ")\n"
         print('Las Vegas')
-    request.session['activities'].append("Hello\n")
+    request.session['activities'].append(string)
     request.session.save()
+    return redirect("/")
+
+def reset(request):
+    request.session.flush()
     return redirect("/")
