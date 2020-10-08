105dee44a2189ac3e57ab6a049cb8c385ef3e35b
Ian Rones <ronesp.85@gmail.com>
2020-10-07 20:51:04 -0700

    [add] dojo ninjas template completed

--- Python/Django/django_orm/dojo_ninjas/dojo_app/urls.py
+++ Python/Django/django_orm/dojo_ninjas/dojo_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add-dojo', views.add_dojo),
+    path('add-ninja', views.add_ninja),
+    path('delete',views.delete),
 ]
--- Python/Django/django_orm/dojo_ninjas/dojo_app/views.py
+++ Python/Django/django_orm/dojo_ninjas/dojo_app/views.py
@@ -1,6 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from .models import Dojo, Ninja
 
+
 def index(request):
-    return render(request,"index.html")
+    context = {
+        'all_dojos':Dojo.objects.all(),
+    }
+    return render(request,"index.html", context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name = request.POST["name"],
+        city = request.POST["city"],
+        state = request.POST["state"],
+    )
+    return redirect("/")
+
+def add_ninja(request):
+    Ninja.objects.create(
+        dojo_id = Dojo.objects.get(id=request.POST["dojo-name"]),
+        first_name = request.POST["fname"],
+        last_name = request.POST["lname"],
+    )
+    return redirect("/")
+
+def delete(request):
+    Dojo.objects.get(id=request.POST["delete"]).delete()
+    return redirect("/")
