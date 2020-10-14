38f853f710640eb9990c1b00de48efe56027e913
Ian Rones <ronesp.85@gmail.com>
2020-10-14 12:46:13 -0700

    [add] validation check added

--- Python/Django/django_fullstack/tv_project/show_app/views.py
+++ Python/Django/django_fullstack/tv_project/show_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 from .models import Shows
 
@@ -9,15 +10,21 @@ def new_show(request):
     return render(request, "add_show.html")
 
 def create_show(request):
-    new_show = Shows.objects.create(
-        title = request.POST["title"],
-        network = request.POST["network"],
-        release_date = request.POST["date"],
-        description = request.POST["description"],
-    )
-    return redirect(f"/shows/{new_show.id}")
-
-def show(request, show_id):
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for _ ,v in errors.items():
+            messages.error(request, v)
+        return redirect("shows/new")
+    else:
+        new_show = Shows.objects.create(
+            title = request.POST["title"],
+            network = request.POST["network"],
+            release_date = request.POST["date"],
+            description = request.POST["description"],
+        )
+        return redirect(f"/shows/{new_show.id}")
+ 
+def show(request, show_id):2
     context = {
         "all_shows" : Shows.objects.all()
     }
@@ -32,13 +39,19 @@ def edit_show(request, show_id):
     return render(request, "edit-show.html", context)
 
 def update_show(request, show_id):
-    show = Shows.objects.get(id=show_id)
-    show.title = request.POST["title"]
-    show.network = request.POST["network"]
-    show.release_date = request.POST["date"]
-    show.description = request.POST["description"]
-    show.save()
-    return redirect(f"shows/{show_id}")
+    errors = Shows.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for _, v in errors.items():
+            messages.error(request, v)
+        return redirect(f"/shows/{show_id}/edit")
+    else:
+        show = Shows.objects.get(id=show_id)
+        show.title = request.POST["title"]
+        show.network = request.POST["network"]
+        show.release_date = request.POST["date"]
+        show.description = request.POST["description"]
+        show.save()
+        return redirect(f"shows/{show_id}")
 
 def destroy_show(request, show_id):
     show = Shows.objects.get(id=show_id)
