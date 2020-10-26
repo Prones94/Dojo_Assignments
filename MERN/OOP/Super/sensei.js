89aec2fdd640f6be62507fdff3d18e9dcb03c171
Ian Rones <ronesp.85@gmail.com>
2020-10-26 14:28:28 -0700

    [add] super sensei assignment completed

---
+++ MERN/OOP/Super/sensei.js
@@ -0,0 +1,38 @@
+class Ninja{
+    constructor(name, health){
+        this.name = name;
+        this.health = health;
+        this.speed = 3
+        this.strength = 3;
+    }
+
+    sayName() {
+        console.log(`My name is ${this.name}`);
+    }
+    showStats() {
+        console.log(`Name: ${this.name}`);
+        console.log(`Strength: ${this.strength}`);
+        console.log(`Speed: ${this.speed}`);
+        console.log(`Health: ${this.health}`);
+    }
+    drinkSake() {
+        console.log('Adding health!');
+        this.health += 10
+    }
+}
+
+class Sensei extends Ninja {
+    constructor(name){
+        super(name, 200);
+        this.speed = 10
+        this.strength = 10
+        this.wisdom = 10
+    }
+    speakWisdom(){
+        this.drinkSake()
+        console.log('What one programmer can do in one month, two programmers can do in two months');
+    }
+}
+const superSensei = new Sensei("Master Splinter")
+superSensei.speakWisdom()
+superSensei.showStats()
