bf5e6040603ebde2cc3d3ea5c19d4c579ef1103c
Ian Rones <ronesp.85@gmail.com>
2020-11-07 05:47:06 -0800

    [edit] pokemon version 1 now has an clear button. Uses useEffect and async/await

--- MERN/React/APIs/pokemon_v1/src/App.js
+++ MERN/React/APIs/pokemon_v1/src/App.js
@@ -1,19 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 function App(props) {
   const API_URL = 'https://pokeapi.co/api/v2/pokemon/?limit=807'
   const [data, setData] = useState([])
 
+  useEffect( () => {
+    handleClick()  
+  }, [])
   const handleClick = async () => {
     const pokePromise = await fetch(API_URL)
     const response = await pokePromise.json()
     setData(response.results)
   }
+
+  const clearAll = e => {
+    setData("")
+  }
   return (
     <div className="App">
       <button onClick={handleClick}>Load Pokemon</button>
-      {data.map((pokemon, i) =>{
+      <button onClick={clearAll}>Clear</button>
+      {data.length > 0 && data.map((pokemon, i) =>{
         return (
           <p key={i}>{pokemon.name}</p>
         )
