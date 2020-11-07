1e211ebe3b86ba444044d5916d98121e8670e294
Ian Rones <ronesp.85@gmail.com>
2020-11-07 06:08:03 -0800

    [add] pokemon assignment using axios, async and await

---
+++ MERN/React/APIs/pokemon_v2/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([])
+  const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=807'
+
+  const handleClick = async () => {
+    const pokemon = await axios.get(API_URL)
+    setData(pokemon.data.results)
+  }
+
+  return (
+    <div className="App">
+      <button onClick={handleClick}>Search</button>
+      {data.map((pokemon, i) => (
+        <p key={i}>{pokemon.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default App;
