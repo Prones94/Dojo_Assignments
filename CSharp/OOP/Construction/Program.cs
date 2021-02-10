5203832ec90a1e5204fb2a1902e9cc741b777483
Ian Rones <ronesp.85@gmail.com>
2021-02-10 02:11:50 -0800

    [add] Human assignment completed

---
+++ CSharp/OOP/Construction/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Construction
+{
+    class Program
+    {
+        /*
+            Object Construction
+        */
+        static void Main(string[] args)
+        {
+            // Instantiation of the Vehicle Class
+            Vehicle myCar = new Vehicle();
+            Console.WriteLine($"This vehicle holds {myCar.NumPassengers} people");
+        }
+
+    }
+
+        // Creating a Vehicle Class outside of the main Program class
+    public class Vehicle
+    {
+        /*
+            Accessibility of class members is defaulted to private
+            Must add the PUBLCI keyword to anything we want to allow access into
+        */
+
+        // Unassigned public integer initialized to 0
+        public int NumPassengers;
+
+        // Creating the Constructor, use the same name as the class
+        // public Vehicle(int val)
+        // {
+        //     NumPassengers = val;
+        // }
+        // We can create a constructor class without passing in values and create them within the constructor itself
+        public Vehicle() // notice nothing within the parens
+        {
+            NumPassengers = 3;
+        }
+    }
+}
