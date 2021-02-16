ea16baaf68facb13095abb8b4041366182c166d4
Ian Rones <ronesp.85@gmail.com>
2021-02-16 11:38:49 -0800

    [add] dojo survey assignment completed

---
+++ CSharp/ASP.NET_Core/DojoSurvey/Controllers/HomeControllers.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DojoSurvey.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("")]
+    public ViewResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("result")]
+    public IActionResult Result(string Name, string Location, string Language, string Comment)
+    {
+      ViewBag.Name= Name;
+      ViewBag.Location = Location;
+      ViewBag.Language = Language;
+      ViewBag.Comment = Comment;
+      return View("Result");
+    }
+  }
+}
