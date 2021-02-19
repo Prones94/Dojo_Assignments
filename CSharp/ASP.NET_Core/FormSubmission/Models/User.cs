36140f5874e913e0bbc2d02e4edd2720f315d8ba
Ian Rones <ronesp.85@gmail.com>
2021-02-18 20:06:56 -0800

    [add] form submission assignment completed

---
+++ CSharp/ASP.NET_Core/FormSubmission/Models/User.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+  public class User
+  {
+    [Required(ErrorMessage="First name is needed")]
+    [MinLength(3, ErrorMessage="Needs to be at least 3 characters long")]
+    [Display(Name="First Name")]
+    public string FirstName {get; set;}
+
+    [Required(ErrorMessage="Last name is needed")]
+    [MinLength(3, ErrorMessage="Needs to be at least 3 characters long")]
+    [Display(Name="Last Name")]
+    public string LastName {get;set;}
+
+    [Required(ErrorMessage="Please enter an age")]
+    [Range(0,100, ErrorMessage="Must be still alive")]
+    [Display(Name="Age")]
+    public int? Age {get; set;}
+
+    [Required(ErrorMessage="Please enter an email")]
+    [EmailAddress(ErrorMessage="This is not a valid email address")]
+    [Display(Name="Email Address")]
+    public string Email{get;set;}
+
+    [Required(ErrorMessage="Please enter a password")]
+    [MinLength(6, ErrorMessage="Must be at least 6 characters long")]
+    [Display(Name="Password")]
+    public string Password {get; set;}
+  }
+}
